8b64b3d4d433208cc7d6bf01187715a038a1e6a0
samyzaitoun <58275644+samyzaitoun@users.noreply.github.com>
2022-06-13 03:37:35 +0300

    Defined some classes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MAPF-Planner
- The implementation of the Planner component in the MAPF project.
+ The implementation of the Planner component in a MAPF project conducted at Technion by Prof. Oren Salzman & under the supervision of CRL lab manager David Dovrat.
+ By: Samy Zaitoun, Oren Rosenberg 
---
+++ utils.py
@@ -0,0 +1,60 @@
+
+
+from abc import ABC
+from typing import Callable
+
+class Topic:
+    """
+    A facade class which is meant to facilitate the publish/subscribe needs of our architecture.
+    """
+
+    def __init__(self, topic) -> None:
+        """
+        Save the topic instance/interface in the class.
+        """
+        pass
+
+    def publish(**kwargs) -> None:
+        """
+        JSON-ify the passed arguments and publish them to the topic.
+        """
+        pass
+
+    def subscribe(func: Callable) -> None:
+        """
+        Subscribe the passed function to the topic, and call it with the published kwargs.
+        IMPORTANT: Don't forget to wrap func with a decorator which un-JSON-ifies the published data, before calling func. 
+        """
+        pass 
+
+
+class BasePlanner(ABC):
+    """
+    Base class for the Planner component.
+    A planner calculates new paths when it's method is called, and publishes them to the executors once it's done.
+    A configurator will create an instance of Planner and hook it's plan method to be triggered whenever something is published by the Manager component
+    If you want to implement a new planner, please inherit from this class (inheritance will be checked).
+    """
+
+    def __init__(self, executors_topic, tf_topic ) -> None:
+        """
+        Init is called once when configurator initiates the system.
+        In init the planner should only take arguments that provide a constant interface throughout the entire execution,
+        instead of having them passed on everytime plan is called.
+        * exeuctors_topic - an instance which provides an interface which the planner could publish through to executors.
+        * 
+        """
+        raise NotImplementedError
+
+    # To be decided - arguments 
+    def plan(*args, **kwargs):
+        raise NotImplementedError
+
+
+class BaseDestinator(ABC):
+    pass
+
+
+class BaseManager(ABC):
+    pass
+
