654a62172c6ffaab99fd2fc85e7bf989ded4c35b
rooren <rooren777@gmail.com>
2022-07-29 02:16:08 -0700

    add goal assigner

--- arch_components/arch_components/planner.py
+++ arch_components/arch_components/planner.py
@@ -1,4 +1,3 @@
-
 from typing import Iterable, List, Tuple, Type
 import rclpy
 from rclpy.node import Node, Publisher, Subscription
@@ -15,26 +14,32 @@ from mapf_solver.Abstract_objects.mapf_solver import MAPFSolver, MAPFInput, MAPF
 from mapf_solver.Abstract_objects.path import Path
 from mapf_solver.Concrete_objects.concrete_waypoints import TimedWayPoint
 
+from goal_assigner import goalAssigner, assigningGoalsException
+
 NO_TIME_LIMIT = -1
+
 
 class Planner(Node):
     """
     The Planner component node implementation
     """
+
     publisher: Publisher = None
     plan_subscription: Subscription = None
     tf_subscription: Subscription = None
-     
+
     def __init__(self):
-        super().__init__('planner_component')
+        super().__init__("planner_component")
 
         # Retrieve launch parameters
         self.get_launch_parameters()
-        
-        # Create Service PlanRequest
-        self.plan_srv = self.create_service(PlanRequest, 'plan_request', self.plan_callback)
 
-        # TODO: Create Publisher to Plan topic 
+        # Create Service PlanRequest
+        self.plan_srv = self.create_service(
+            PlanRequest, "plan_request", self.plan_callback
+        )
+
+        # TODO: Create Publisher to Plan topic
         # self.publisher = self.create_publisher(Paths, path_topic, 1)
 
         # Calculate constant values
@@ -48,45 +53,62 @@ class Planner(Node):
         # Initialize algorithm solver class
         self.mapf_solver = self.mapf_solver_class(self.time_limit)
 
+        # set goal assigning algorithem
+        self.assign_goals_to_agents = self.goal_assigner_class.assign_goals_to_agents
+
     def get_launch_parameters(self) -> None:
-        
+
         ##  Arena transform tf name (str)
-        self.declare_parameter('tf_tag_arena', 'arena')
-        self.arena_frame: str = self.get_parameter(
-            'tf_tag_arena').get_parameter_value().string_value
-        
+        self.declare_parameter("tf_tag_arena", "arena")
+        self.arena_frame: str = (
+            self.get_parameter("tf_tag_arena").get_parameter_value().string_value
+        )
+
         ## Arena height (int)
-        self.declare_parameter('arena_height')
-        self.arena_height: int = self.get_parameter(
-            'arena_height').get_parameter_value().integer_value
+        self.declare_parameter("arena_height")
+        self.arena_height: int = (
+            self.get_parameter("arena_height").get_parameter_value().integer_value
+        )
 
         ## Arena width (int)
-        self.declare_parameter('arena_width')
-        self.arena_width: int = self.get_parameter(
-            'arena_width').get_parameter_value().integer_value
-        
+        self.declare_parameter("arena_width")
+        self.arena_width: int = (
+            self.get_parameter("arena_width").get_parameter_value().integer_value
+        )
+
         ## Agent diameter (int)
-        self.declare_parameter('agent_diameter')
-        self.agent_diameter: int = self.get_parameter(
-            'agent_diameter').get_parameter_value().integer_value
-        
+        self.declare_parameter("agent_diameter")
+        self.agent_diameter: int = (
+            self.get_parameter("agent_diameter").get_parameter_value().integer_value
+        )
+
         ## MAPF Algorithm Class (MAPFSolver)
-        self.declare_parameter('mapf_solver')
+        self.declare_parameter("mapf_solver")
         self.mapf_solver_class: Type[MAPFSolver] = self.get_parameter(
-            'mapf_solver').get_parameter_value() # Not Defined, should work..?
+            "mapf_solver"
+        ).get_parameter_value()  # Not Defined, should work..?
+
+        # copied from  MAPF Algorithm Class, TODO: check it works and delete this comment
+        ## goal assign Algorithm Class (goal_assigner)
+        self.declare_parameter("goal_assigner")
+        self.goal_assigner_class: Type[goalAssigner] = self.get_parameter(
+            "goal_assigner"
+        ).get_parameter_value()  # Not Defined, should work..?
 
         ## MAPF Algorithm Input Class (MAPFInput)
-        self.declare_parameter('mapf_input')
+        self.declare_parameter("mapf_input")
         self.mapf_input_class: Type[MAPFInput] = self.get_parameter(
-            'mapf_input').get_parameter_value() # Not Defined, should work..?
-        
+            "mapf_input"
+        ).get_parameter_value()  # Not Defined, should work..?
+
         ## MAPF Algorithm Time limit (float)
-        self.declare_parameter('time_limit', NO_TIME_LIMIT)
-        self.time_limit: float = self.get_parameter(
-            'time_limit').get_parameter_value().double_value
+        self.declare_parameter("time_limit", NO_TIME_LIMIT)
+        self.time_limit: float = (
+            self.get_parameter("time_limit").get_parameter_value().double_value
+        )
 
     def plan_callback(self, request, response):
-        
+
         # Extract message arguments
         assigned_goals: List[AssignedGoal] = request.assigned_goals
         unassigned_goals: Iterable[Position] = request.unassigned_goals
@@ -94,10 +116,19 @@ class Planner(Node):
 
         # Assign goals
         # TODO: Define & Implement assignment class
-        assigned_goals += self.assign_goals_to_agents(unassigned_goals, unassigned_agents)
+
+        try:
+            assigned_goals += self.assign_goals_to_agents(
+                unassigned_goals, unassigned_agents
+            )
+        except assigningGoalsException as ex:
+            response.error_msg = str(ex)
+            return response
 
         # Unpack message layers (not fun to work with 2 layers of indirection)
-        assigned_goals: List[Tuple[str, Position]] = [(m.agent_id, m.pos) for m in assigned_goals]
+        assigned_goals: List[Tuple[str, Position]] = [
+            (m.agent_id, m.pos) for m in assigned_goals
+        ]
 
         # TODO: Understand how to pull obstacle transformations
         obstacle_transformations = []
@@ -108,52 +139,42 @@ class Planner(Node):
             now = rclpy.time.Time()
             for agent_id, _ in assigned_goals:
                 agent_transformations += self.tf_buffer.lookup_transform(
-                    agent_id,
-                        self.arena_frame,
-                        now   
+                    agent_id, self.arena_frame, now
                 )
         except TransformException as ex:
             self.get_logger().info(
-                f'Could not transform {agent_id} to {self.arena_frame}: {ex}')
+                f"Could not transform {agent_id} to {self.arena_frame}: {ex}"
+            )
             return
 
         # Solve MAPF using data
         try:
             mapf_solution = self.generate_and_solve_map(
-                agent_transformations, 
-                assigned_goals, 
-                obstacle_transformations
+                agent_transformations, assigned_goals, obstacle_transformations
             )
         except Exception as e:
             # Just pointing this out that the library throws errors directly inherited from Exception (!!!)
             # Someone please fix their inheritance to AssertionError!
             response.error_msg = str(e)
             return response
-        
+
         agent_paths: List[Path] = mapf_solution.paths
-        
+
         # TODO:
         # A. Figure out what message type we agree on
         # B. Transform the paths from our Discretized map to scene transforms
         # (Remember to add them relative to mocap's transform)
         # C. Publish them, and return a 'success' response to whoever initiated the plan request.
-        
+
         # msg_publish = Paths()
         # msg_publish.paths = paths
         # self.publisher.publish(msg_publish)
-
-    def assign_goals_to_agents(
-        self, 
-        goals: Iterable[Position], 
-        agents: Iterable[str]
-    ) -> List[AssignedGoal]:
-        pass
 
     def generate_and_solve_map(
         self,
         agents: List[TransformStamped],
         goals: List[Tuple[str, Position]],
-        obstacles: List[TransformStamped]
+        obstacles: List[TransformStamped],
     ) -> MAPFOutput:
         """
         In our lab enviornement, our "relative" view point captures & publishes agent transformations
@@ -166,23 +187,32 @@ class Planner(Node):
                       5 |
                       4 |           * Beta_G
                       3 | * Alpha_A
-                      2 |       
+                      2 |
                       1 |
             -> mocap  0 -------------->
                         0  1  2  3  4
-        """             
+        """
         # Discretize agent, goal & obstacle positions into board
         discrete_agents = [
-            WayPoint(x=agent.x/self.agent_diameter, y=agent.z/self.agent_diameter) 
+            WayPoint(
+                x=int(agent.x / self.agent_diameter),
+                y=int(agent.z / self.agent_diameter),
+            )
             for agent in agents
         ]
         # Algo lib uses TimedWayPoint for goals and I don't understand why. (It doesn't supply a t value...)
         discrete_goals = [
-            TimedWayPoint(x=goal[1].x/self.agent_diameter, y=goal[1].y/self.agent_diameter) 
+            TimedWayPoint(
+                x=int(goal[1].x / self.agent_diameter),
+                y=int(goal[1].y / self.agent_diameter),
+            )
             for goal in goals
         ]
         discrete_obstacles = [
-            (obstacle.x/self.agent_diameter, obstacle.z/self.agent_diameter) 
+            (
+                int(obstacle.x / self.agent_diameter),
+                int(obstacle.z / self.agent_diameter),
+            )
             for obstacle in obstacles
         ]
         obstacle_map = self.create_empty_map()
@@ -190,14 +220,14 @@ class Planner(Node):
             # X is col, Y is row (under our representation)
             obstacle_row, obstacle_col = obstacle[1], obstacle[0]
             obstacle_map[obstacle_row][obstacle_col] = True
-        
+
         # Initialize algorithm input
         mapf_input: MAPFInput = self.mapf_input_class(
-            map_instance=obstacle_map, 
-            starts_list=discrete_agents, 
-            goals_list=discrete_goals
+            map_instance=obstacle_map,
+            starts_list=discrete_agents,
+            goals_list=discrete_goals,
         )
-        
+
         # The following method call can throw (up)
         return self.mapf_solver(mapf_input)
 
@@ -207,4 +237,3 @@ class Planner(Node):
         In the library, a 2D list of bools represent empty & taken positions (False = Empty, True = Taken)
         """
         return [[False] * self.cols] * self.rows
-    
